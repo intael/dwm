134e3fd9fe5e36b69dabd8787daf75d171d71e84
intael <carlosvalde9@gmail.com>
2025-02-21 18:27:36 +0200

    use picom instead of xcompmgr

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-xrandr --output DP-2 --mode 1920x1080 --primary --rate 164.92 --output DVI-I-0 --right-of DP-2
+xrandr --output DP-2 --mode 1920x1080 --primary --rate 164.92 #--output DVI-I-0 --right-of DP-2
 WALLPAPERS=~/Pictures/wallpapers
 feh --bg-fill --randomize $WALLPAPERS/*
 killall slstatus
 slstatus &
-killall xcompmgr
-xcompmgr -c &
+killall picom
+picom --config $HOME/.config/picom/picom.conf -b
 xscreensaver -no-splash -verbose &
 greenclip daemon &
